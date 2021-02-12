f6c5bb40c6bb5fa1978497f63341ec41f91e3c33
SatrioAjiTI <78513122+SatrioAjiTI@users.noreply.github.com>
2021-02-12 14:28:39 +0700

    Add files via upload

---
+++ Vid 1.py
@@ -0,0 +1,7 @@
+#Satrio Aji Utomo 71200538
+"""
+Agus ingin menghitug pendapatan dan pengeluarannya. Dalam satu bulan gaji Agus dihitung dari 
+jumlah hari ia masuk. Dari gaji yang dia dapat 11% akan dia tabung, Setelah menabung 13.5% gaji Agus 
+digunakan untuk membayar asuransi anggota keluarga nya lalu 17% akan di berikan pada orang tua 
+nya dan 7% akan disumbangkan pada unit penanggulangan bencana.
+
